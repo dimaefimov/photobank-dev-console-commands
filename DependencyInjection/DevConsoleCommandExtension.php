bab3e6ea3e619ee954aa940bcb883a0feb3dc185
Unknown <dmitriy.efimov@yahoo.com>
2019-02-07 18:16:22 +0300

    Added extension class

---
+++ DependencyInjection/DevConsoleCommandExtension.php
@@ -0,0 +1,22 @@
+<?php
+namespace PhotoBank\DevConsoleCommandBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DevConsoleCommandExtension extends Extension
+{
+  public function load(array $configs, ContainerBuilder $container)
+  {
+    $loader = new XmlFileLoader(
+      $container,
+      new FileLocator(__DIR__.'/../Resources/config')
+    );
+    $loader->load('services.xml');
+
+    //$configuration = new Configuration();
+    //$config = $this->processConfiguration($configuration, $configs);
+  }
+}
